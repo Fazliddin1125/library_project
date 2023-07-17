c89f580c69ae7b8efea7d834b308bb867dcf02db
Fazliddin <fazliddinbahromov9717@gmail.com>
2023-07-17 23:21:43 +0500

    genicorn

--- librariy/settings.py
+++ librariy/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vhvr7o_=ief37@qk!#uy()76)_x%aez$x5+5scg&dgp&0tg-5j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.98.172', 'http:// 192.168.98.172:8000', 'http:// 192.168.98.172', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -141,6 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
